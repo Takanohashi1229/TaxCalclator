085a84d073468083a5e232be1595509be3d819aa
kyohei <takanohashi.com@gmail.com>
2023-09-12 19:48:06 +0900

    TestFile

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomNumber = 1
+    @State private var timer: Timer?
+    @State private var inRolling = false
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            Image(systemName: "die.face.\(randomNumber)")
+                .resizable()
+                .scaledToFit()
+                .frame(width: UIScreen.main.bounds.width/2)
+                .padding()
+            Spacer()
+            Button {
+               playDice()
+            } label: {
+                Text("サイコロを振る")
+                    .padding()
+                    .background(Color.orange)
+                    .foregroundColor(.black)
+                    .cornerRadius(10)
+            }
+            .disabled(inRolling)
+            Spacer()
         }
         .padding()
+    }
+    private func playDice() {
+        print("ボタンが押されたよ")
+        inRolling = true
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            randomNumber = Int.random(in: 1...6)
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
+            timer?.invalidate()
+            timer = nil
+            inRolling = false
+        }
     }
 }
 
