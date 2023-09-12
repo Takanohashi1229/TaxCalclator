eda3679518abb5a4d6dbd86ce9e8700591c4a707
kyohei <takanohashi.com@gmail.com>
2023-09-12 10:29:03 +0900

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by 鷹觜恭平 on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
